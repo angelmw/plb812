d02751b60f5611017da602f96b4ba76361eeea75
angelmw <mckayang@msu.edu>
2022-09-28 14:00:45 -0400

    Assignment2

---
+++ lab_assignments/assignment2/run_fastqc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash --login
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100GB
+#SBATCH --job-name assignment-2
+#SBATCH --output=%x-%j.SLURMout
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mckayang@msu.edu
+
+cd ${PBS_O_WORKDIR}
+
+#Add conda environment to the path
+export PATH="${HOME}/miniconda3/envs/plb812/bin:${PATH}"
+export LD_LIBRARY_PATH="${HOME}/miniconda3/envs/plb812/lib:${LD_LIBRARY_PATH}"
+
+fastqc -f fastq -o ${HOME}/plb812/lab_assignments/assignment2 ${HOME}/plb812/data/RNA-seq/*.fastq.gz
