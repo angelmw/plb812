4591818f1c01a673edde99870a1583a58e1d42a4
angelmw <mckayang@msu.edu>
2022-10-18 14:15:36 -0400

    assignment4

--- lab_assignments/assignment4/alignment.sh
+++ lab_assignments/assignment4/alignment.sh
@@ -2,9 +2,9 @@
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=100GB
-#SBATCH --job-name assignment-3
+#SBATCH --job-name assignment-4
 #SBATCH --output=%x-%j.SLURMout
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mckayang@msu.edu
@@ -20,11 +20,5 @@ sra_list="SRR5448223 SRR5448224 SRR5448225 SRR5448226 SRR5448227 SRR5448228 SRR5
 #trim
 for sra in ${sra_list}
 do
-trimmomatic PE -threads 13 -phred33 ${sra}_1.fastq.gz ${sra}_2.fastq.gz ${sra}_1_trimmed.fastq.gz ${sra}_2_trimmed.fastq.gz LEADING:20 TRAILING:20 MINLEN:30
+hisat2 -p 20 -x ${HOME}/plb812/data/St_Phytozome/annotation/St_assembly -1 ${HOME}/plb812/data/RNA-seq/${sra}_p1_trimmed.fastq.gz -2 ${HOME}/plb812/data/RNA-seq/${sra}_p2_trimmed.fastq.gz | samtools view -@ 20 -bh | samtools sort -@ 20 > ${HOME}/plb812/data/RNA-seq_alignment/${sra}.bam 
 done
-
-#fastqc
-fastqc -f fastq -o ${HOME}/plb812/data/RNA-seq_trimmed_fastqc ${HOME}/plb812/data/RNA-seq/*trimmed.fastq.gz
-
-#copy html to assignment3
-cp ${HOME}/plb812/data/RNA-seq_trimmed_fastqc/*fastqc.html ${HOME}/plb812/lab_assignments/assignment3
